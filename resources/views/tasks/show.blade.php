909f7fb1441395710fd6d2ca12861d8293f57bd8
binaya-khadka <binayamm@gmail.com>
2024-02-24 12:57:49 +0545

    working ui ux

--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -1,11 +1,35 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1> Title </h1>
-    <p> {{ $task->title }} </p>
-    <h1> Description </h1>
-    <p> {{ $task->description }} </p>
-    <h3>
-        <a href="{{ route('edit_task', $task->id) }}"> Edit this Task</a>
-    </h3>
+    <div>
+        <a href="{{ route('create_task') }}" class="btn btn-primary float-right mt-5 mb-5">
+            Add tasks
+        </a>
+        <table class="table w-100">
+            <thead>
+                <tr>
+                    <th scope="col" style="width:20%">Title</th>
+                    <th scope="col" style="width:40%">Description</th>
+                    <th scope="col" style="width:20%">Completed</th>
+                    <th scope="col" style="width: 20%">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>{{ $task->title }}</td>
+                    <td>{{ $task->description }}</td>
+                    <td>
+                        @if ($task->is_completed)
+                            <span class="badge badge-success">Completed</span>
+                        @else
+                            <span class="badge badge-warning">Not completed</span>
+                        @endif
+                    </td>
+                    <td>
+                        <a class="btn btn-primary" href="{{ route('edit_task', $task->id) }}">Edit</a>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 @endsection
